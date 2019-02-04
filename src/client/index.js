097eb9ad9943612b7c5556f324ad842c91661cdd
Alan Maldonado <alan.maldonado@wizeline.com>
2019-02-04 13:55:22 -0600

    ♻️ Refactoring to pages

--- src/client/index.js
+++ src/client/index.js
@@ -11,7 +11,11 @@ import reducers from './reducers';
 
 import Routes from './Routes';
 
-const store = createStore(reducers, {}, applyMiddleware(thunk));
+const store = createStore(
+  reducers,
+  window.INITIAL_STATE,
+  applyMiddleware(thunk)
+);
 
 ReactDOM.hydrate(
   <Provider store={store}>
--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <div>
-        Home AMAZING component
-      </div>
+      <div>Home AMAZING component</div>
       <button onClick={() => console.log('Hi there!')}>Press me!</button>
     </div>
   );
 };
 
-export default Home;
+export default {
+  component: Home
+};
